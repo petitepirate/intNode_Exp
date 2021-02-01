941b0252451a56079c257769fafe0f7d9e27db2c
megan <msmeganmcmanus@gmail.com>
2021-02-01 15:22:17 -0800

    start exercise with starter code

---
+++ bookstoreEx/data.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS books;
+
+CREATE DATABASE books;
+
+\c books;
+
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+  isbn TEXT PRIMARY KEY,
+  amazon_url TEXT,
+  author TEXT,
+  language TEXT, 
+  pages INTEGER,
+  publisher TEXT,
+  title TEXT, 
+  year INTEGER
+);
